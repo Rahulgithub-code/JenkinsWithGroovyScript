d09e9480d965736eba61b4b818a7ec27f7c9d29a
Rahul Kumar Ram <36235025+Rahulgithub-code@users.noreply.github.com>
2020-07-05 14:21:53 -0400

    Update code.groovy

--- code.groovy
+++ code.groovy
@@ -24,8 +24,11 @@ job("task6-job1") {
 job("task6-job2"){
   description("This job will pull the github repo on every push, update the container using given Dockerfile and push image to DockerHub")
   steps {
-    shell('sudo kubectl run os --image httpd')
+    shell('''sudo kubectl run os --image httpd
+             sudo kubectl expose pods os --type=NodePort --port=80     
+    ''')
   }
+  
   triggers {
         		upstream('task6-job1', 'SUCCESS')
   }
