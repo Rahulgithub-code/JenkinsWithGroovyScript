f1c001f015cc34cd309a7e11cc5f8c8b5ba9257d
Rahul Kumar Ram <36235025+Rahulgithub-code@users.noreply.github.com>
2020-07-05 13:32:01 -0400

    Update code.groovy

--- code.groovy
+++ code.groovy
@@ -1,9 +1,8 @@
 job("task6-job1") {
   description("This job will pull the github repo on every push, update the container using given Dockerfile and push image to DockerHub")
-  
   scm {
     github('Rahulgithub-code/JenkinsWithGroovyScript','master')
-  }
+      }
   
   steps {
     shell('sudo cp -vrf * /root/task6')
@@ -24,7 +23,10 @@ job("task6-job1") {
 
 job("task6-job2"){
   description("This job will pull the github repo on every push, update the container using given Dockerfile and push image to DockerHub")
-}
-steps {
+  steps {
     shell('sudo kubectl run os --image httpd')
   }
+  triggers {
+        		upstream('task6-job1', 'SUCCESS')
+  }
+}
